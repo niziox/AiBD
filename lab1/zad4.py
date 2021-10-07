6f3c5c0fdd24166e0b38e55f9d0a13fc7c57bece
Arkadiusz Kontek <arek8131@op.pl>
2021-10-07 21:13:33 +0200

    add comments zad4

--- lab1/zad4.py
+++ lab1/zad4.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import numpy as np
 
+# inicjalizacja danych
 data = np.array([['Arkadiusz', 'Kontek', 21, 'male'], ['Maciej', 'Gdzik', 34, 'male'],
                  ['Aleksandra', 'Podkowa', 18, 'female'], ['Robert', 'Kowak', 66, 'male'],
                  ['Agnieszka', 'Martin', 30, 'female']])
 
+# utworzenie DataFrame
 df = pd.DataFrame(data=data, columns=['name', 'surname', 'age', 'sex'], index=np.arange(1, len(data)+1))
 
+# wyświetlenie informacji o danych
 print(df.info())
 
-display(df.describe())
+# wyświetlenie opisu danych danych
+print(df.describe())
 
-display(df.head(3))
+# wyświetlenie trzech pierwszych rekordów
+print(df.head(3))
